da6880d9f0926d8749f7c0983f0ab929f8474d7e
Ruoqi Yang <ry@cs.ualberta.ca>
2019-01-08 19:45:04 -0700

    message

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/models/Circle.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/models/Circle.java
@@ -1,8 +1,18 @@
 package ca.ualberta.cs.lonelytwitter.models;
 
 public class Circle extends GraphicObject {
+    private int radius;
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
     public void draw(){
         // some her to draw the circle
     }
-    
+
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/models/GraphicObject.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/models/GraphicObject.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.lonelytwitter.models;
 public abstract class GraphicObject {
     private int x;
     private int y;
+
     GraphicObject(){
         this.x = 0;
         this.y = 0;
